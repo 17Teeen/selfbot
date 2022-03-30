6d4cc5591137e3141d14e44ff364ed12e8b32bb7
Ben Tettmar <bentettmar@yahoo.com>
2022-03-30 20:04:03 +0100

    Notifications util

---
+++ utils/notifier.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+
+class Notifier:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def send(title, text) -> None:
+        if sys.platform == "win32":
+            import win10toast
+            toast = win10toast.ToastNotifier()
+            toast.show_toast(title, text, duration=5)
+        elif sys.platform == "linux":
+            subprocess.Popen(["notify-send", title, text])
+        elif sys.platform == "darwin":
+            cmd = '''on run argv
+  display notification (item 2 of argv) with title (item 1 of argv)
+end run'''
+            subprocess.call(['osascript', '-e', cmd, title, text])
